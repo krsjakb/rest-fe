fd7f84a081f9216a5028cba7561a959067f91016
vparon <Vpa.2000.szolgalat@gmail.com>
2022-01-03 15:34:00 +0100

    Az én munkám
    -

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import axios from "axios";
-import VueAxios from 'vue-axios';
+import VueAxios from "vue-axios";
 Vue.use(VueAxios, axios);
 
 Vue.use(BootstrapVue);
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,11 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import CarBrand from "../views/CarBrand.vue";
-import AddCarBrand from "../views/AddCarBrand.vue"
-import EditBrand from "../views/EditBrand.vue"
+import AddCarBrand from "../views/AddCarBrand.vue";
+import EditBrand from "../views/EditBrand.vue";
+import AddRandom from "../views/Random/AddRandom";
+import EditRandom from "../views/Random/EditRandom";
+import Random from "../views/Random/Random";
 
 Vue.use(VueRouter);
 
@@ -22,18 +25,33 @@ const routes = [
   {
     path: "/carBrands",
     name: "CarBrands",
-    component: CarBrand
+    component: CarBrand,
   },
   {
     path: "/addCarBrand",
     name: "AddCarBrand",
-    component: AddCarBrand
+    component: AddCarBrand,
   },
   {
     path: "/editBrand/:id",
     name: "EditBrand",
-    component: EditBrand
-  }
+    component: EditBrand,
+  },
+  {
+    path: "/randoms",
+    name: "Randoms",
+    component: Random,
+  },
+  {
+    path: "/editRandom/:id",
+    name: "EditRandom",
+    component: EditRandom,
+  },
+  {
+    path: "/addRandom",
+    name: "AddRandom",
+    component: AddRandom,
+  },
 ];
 
 const router = new VueRouter({
