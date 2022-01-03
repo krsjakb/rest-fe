7280e9fc6980d193d7ec54e6e8d12051b1e99911
pitzrobi <75953140+pitzrobi@users.noreply.github.com>
2022-01-03 22:23:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import CarBrand from "../views/CarBrand.vue";
+import AddCarBrand from "../views/AddCarBrand.vue";
+import EditBrand from "../views/EditBrand.vue";
+import Soundcard from "../views/Soundcard.vue";
+import EditSoundcard from "../views/EditSoundcard.vue";
+import AddSoundcard from "../views/AddSoundcard.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/carBrands",
+    name: "CarBrands",
+    component: CarBrand
+  },
+  {
+    path: "/addCarBrand",
+    name: "AddCarBrand",
+    component: AddCarBrand
+  },
+  {
+    path: "/editBrand/:id",
+    name: "EditBrand",
+    component: EditBrand
+  },
+  {
+    path: "soundcard",
+    name: "Soundcard",
+    component: Soundcard
+  },
+  {
+    path: "/addSoundcard/:id",
+    name: "AddSoundcard",
+    component: AddSoundcard
+  },
+  {
+    path: "/editSoundcard/:id",
+    name: "EditSoundcard",
+    component: EditSoundcard
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
